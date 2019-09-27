b4d020086c6365062f72250c96a142ff5e027d6a
Mike Hansen <mahanse@sandia.gov>
2019-09-26 23:24:02 -0600

    add one more copyright statement

--- spitfire/griffon/griffon.pyx
+++ spitfire/griffon/griffon.pyx
@@ -1,6 +1,14 @@
 # distutils: language = c++
 # cython: language_level=3
 
+# Spitfire - a Python-C++ library for building tabulated chemistry models and solving differential equations                    
+# Copyright 2019 National Technology & Engineering Solutions of Sandia, LLC (NTESS). 
+#                       
+# You should have received a copy of the 3-clause BSD License                                        
+# along with this program.  If not, see <https://opensource.org/licenses/BSD-3-Clause>.   
+#                    
+# Questions? Contact Mike Hansen (mahanse@sandia.gov)    
+
 import numpy as np
 cimport numpy as np
 
