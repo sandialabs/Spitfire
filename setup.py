5a50ef7529f2167fe22053102e84caa40a5ced4a
Mike Hansen <mahanse@sandia.gov>
2021-11-11 13:02:36 -0700

    remove extraneous print

--- setup.py
+++ setup.py
@@ -46,8 +46,6 @@ class ConfigSQA:
       tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
       tag_dict = dict({t: repo.commit(t) for t in tags})
 
-      print(tag_dict)
-
       latest_tag = tags[-1]
       tag_hexsha = tag_dict[latest_tag].hexsha
       is_dirty = repo.is_dirty()
